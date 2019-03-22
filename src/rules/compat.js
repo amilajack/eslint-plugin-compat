d1db2960f7d35e4d5fe2aee70294c69ea3f59c6a
Amila Welihinda <amilajack@gmail.com>
2019-03-21 18:29:18 -0700

    fix linter rules

--- src/rules/compat.js
+++ src/rules/compat.js
@@ -39,7 +39,9 @@ export default {
     // Determine lowest targets from browserslist config, which reads user's
     // package.json config section. Use config from eslintrc for testing purposes
     const browserslistConfig: BrowserListConfig =
-      context.settings.browsers || context.settings.targets || context.options[0];
+      context.settings.browsers ||
+      context.settings.targets ||
+      context.options[0];
 
     const browserslistTargets = Versioning(
       DetermineTargetsFromConfig(browserslistConfig)
