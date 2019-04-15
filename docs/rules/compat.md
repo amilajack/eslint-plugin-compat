7ab234f951a568d721907c2e75ff0ce6f3cc54d2
Julien Fontanet <julien.fontanet@isonoe.net>
2019-04-15 18:54:52 +0200

    add link to rule documentation (#222)
    * add link to rule documentation
    
    Fixes #221
    
    * lint
    
    * add rule doc
    
    * Update compat.md

---
+++ docs/rules/compat.md
@@ -0,0 +1,17 @@
+# compat/compat
+
+This rule enables linting your code for browser compatibility.
+
+## This will be reported
+
+```js
+// Targeting IE
+fetch('https://exmaple.com');
+```
+
+## This will not be reported
+
+```js
+// Using default targets
+fetch('https://exmaple.com');
+```
--- src/rules/compat.js
+++ src/rules/compat.js
@@ -46,6 +46,8 @@ export default {
     docs: {
       description: 'Ensure cross-browser API compatibility',
       category: 'Compatibility',
+      url:
+        'https://github.com/amilajack/eslint-plugin-compat/blob/master/docs/rules/compat.md',
       recommended: true
     },
     fixable: 'code',
