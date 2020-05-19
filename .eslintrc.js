8ff518a23090ac2ad2c7b57e0cd93f0ffb08cebf
John Tran <jptran318@gmail.com>
2020-05-18 22:53:51 -0700

    add typescript to eslint and babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,26 @@ module.exports = {
     node: true,
     jest: true,
   },
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "bliss",
+        "airbnb-typescript/base",
+        "plugin:import/typescript",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+      ],
+      env: {
+        node: true,
+        jest: true,
+      },
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  ],
 };
