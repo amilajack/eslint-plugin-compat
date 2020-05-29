f39f179054d347854156894c1f73ddd73faf5655
John Tran <jptran318@gmail.com>
2020-05-28 20:21:12 -0700

    script fix for eslint@5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,19 @@
 module.exports = {
-  extends: "bliss",
   env: {
     node: true,
     jest: true,
   },
   root: true,
-  overrides: [
-    {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
-      extends: [
-        "bliss",
-        "airbnb-typescript/base",
-        "plugin:import/typescript",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
-      ],
-      env: {
-        node: true,
-        jest: true,
-      },
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "bliss",
+    "airbnb-typescript/base",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
   ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
