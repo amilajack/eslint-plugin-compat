e84bebc150fc2d996a444798999c4b5949236ff1
John Tran <jptran318@gmail.com>
2020-06-11 18:48:48 -0700

    Introduce eslint in benchmarks

--- test/benchmarks.ts
+++ test/benchmarks.ts
@@ -1,39 +1,49 @@
 /* eslint no-console: "off" */
-import { Repository, Clone } from "nodegit";
 import { constants as fsConstants, promises as fs } from "fs";
+import { cwd } from "process";
+import { Repository, Clone } from "nodegit";
+import { ESLint } from "eslint";
 
 type RepoInfo = {
   name: string;
+  location: string;
   remoteLink: string;
   targetGitRef: string;
+  filePatterns: Array<string>;
+  eslintOptions: ESLint.Options;
 };
+
+const projectRoot = cwd();
 
 const repos: Array<RepoInfo> = [
   {
     name: "bootstrap",
+    location: `${projectRoot}/benchmarks-tmp/bootstrap`,
     remoteLink: "https://github.com/twbs/bootstrap.git",
     targetGitRef: "v4.5.0",
-  },
-  {
-    name: "electron-react-boilerplate",
-    remoteLink:
-      "https://github.com/electron-react-boilerplate/electron-react-boilerplate.git",
-    targetGitRef: "v1.1.0",
+    filePatterns: ["js/src", "js/tests", "build/"],
+    eslintOptions: {
+      cwd: `${projectRoot}/benchmarks-tmp/bootstrap`,
+      useEslintrc: false,
+      baseConfig: {
+        extends: ["plugin:compat/recommended"],
+        parser: "babel-eslint",
+      },
+    },
   },
 ];
 
-async function getRepo({ name, remoteLink }: RepoInfo) {
+async function getRepo({ remoteLink, location }: RepoInfo) {
   // Clone if necessary, else use existing repo
-  const repoPath = `benchmarks-tmp/${name}`;
   return fs
-    .access(`benchmarks-tmp/${name}/.git`, fsConstants.R_OK)
+    .access(`${location}/.git`, fsConstants.R_OK)
     .then(() => {
-      console.log(`Using existing ${repoPath}`);
-      return Repository.open(repoPath);
+      console.log(`Using existing ${location}`);
+      return Repository.open(location);
     })
     .catch(() => {
       console.log(`Cloning ${remoteLink}`);
-      return Clone.clone(remoteLink, repoPath);
+      return Clone.clone(remoteLink, location);
     });
 }
 
@@ -41,7 +51,14 @@ async function benchmark(repoInfo: RepoInfo) {
   console.log(`Benchmarking ${repoInfo.remoteLink}`);
   const repo = await getRepo(repoInfo);
   const ref = await repo.getReference(repoInfo.targetGitRef);
-  repo.checkoutRef(ref);
+  const eslint = new ESLint(repoInfo.eslintOptions);
+  console.log(`Checking out ${repoInfo.name} ${repoInfo.targetGitRef}`);
+  await repo.checkoutRef(ref);
+  console.log(`Running ESLint on ${repoInfo.name}`);
+  const lintResults = await eslint.lintFiles(repoInfo.location);
+  lintResults.forEach((res) => {
+    console.log(res);
+  });
 }
 
 (async function main() {
