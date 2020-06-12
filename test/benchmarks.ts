49052c6dc4af89b960bec30d7a7a511c35f35a83
John Tran <jptran318@gmail.com>
2020-06-11 17:12:29 -0700

    Cleanup benchmarks

--- test/benchmarks.ts
+++ test/benchmarks.ts
@@ -1,10 +1,51 @@
 /* eslint no-console: "off" */
-import { Repository } from "nodegit";
+import { Repository, Clone } from "nodegit";
+import { constants as fsConstants, promises as fs } from "fs";
+
+type RepoInfo = {
+  name: string;
+  remoteLink: string;
+  targetGitRef: string;
+};
+
+const repos: Array<RepoInfo> = [
+  {
+    name: "bootstrap",
+    remoteLink: "https://github.com/twbs/bootstrap.git",
+    targetGitRef: "v4.5.0",
+  },
+  {
+    name: "electron-react-boilerplate",
+    remoteLink:
+      "https://github.com/electron-react-boilerplate/electron-react-boilerplate.git",
+    targetGitRef: "v1.1.0",
+  },
+];
+
+async function getRepo({ name, remoteLink }: RepoInfo) {
+  // Clone if necessary, else use existing repo
+  const repoPath = `benchmarks-tmp/${name}`;
+  return fs
+    .access(`benchmarks-tmp/${name}/.git`, fsConstants.R_OK)
+    .then(() => {
+      console.log(`Using existing ${repoPath}`);
+      return Repository.open(repoPath);
+    })
+    .catch(() => {
+      console.log(`Cloning ${remoteLink}`);
+      return Clone.clone(remoteLink, repoPath);
+    });
+}
+
+async function benchmark(repoInfo: RepoInfo) {
+  console.log(`Benchmarking ${repoInfo.remoteLink}`);
+  const repo = await getRepo(repoInfo);
+  const ref = await repo.getReference(repoInfo.targetGitRef);
+  repo.checkoutRef(ref);
+}
 
 (async function main() {
-  const repo = await Repository.open("./benchmarks-tmp/bootstrap");
-  const ref = await repo.getReference("v4.5.0");
-  await repo.checkoutRef(ref);
+  repos.forEach((repo) => benchmark(repo));
 })().catch((e) => {
   console.error(e);
 });
